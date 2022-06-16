090ae35ba7dbd726a5c49cd988c5a36046f7c1fe
Khánh Pluto <tranvankhanh99a@gmail.com>
2022-06-16 23:15:45 +0700

    Add chatbot hugging face

---
+++ chatbot/blenderbot.py
@@ -0,0 +1,17 @@
+from transformers import BlenderbotForConditionalGeneration, BlenderbotTokenizer
+
+
+class BlenderBot(object):
+    def __init__(self):
+        name = "facebook/blenderbot-400M-distill"
+        self.model = BlenderbotForConditionalGeneration.from_pretrained(name)
+        self.tokenizer = BlenderbotTokenizer.from_pretrained(name)
+        self.history = ""
+
+    def __call__(self, message: str, *args, **kwargs):
+        self.history = self.history + message
+        inputs = self.tokenizer([self.history], return_tensors="pt")
+        reply_ids = self.model.generate(**inputs)
+        reply_str = self.tokenizer.batch_decode(reply_ids, skip_special_tokens=True)[0]
+        self.history += "</s> <s>" + reply_str
+        return reply_str
